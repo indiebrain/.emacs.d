f3c6cfd821498e8810b2aca7ed0948e2cfd44520
Aaron Kuehler <aaron.kuehler@gmail.com>
2015-08-14 15:33:25 -0400

    update package repositories

--- configs/001-initalize-package-repositories.el
+++ configs/001-initalize-package-repositories.el
@@ -1,14 +1,8 @@
 (require 'package)
-
-;; Add the original Emacs Lisp Package Archive
-(add-to-list 'package-archives
-             '("elpa" . "http://elpa.gnu.org/packages/"))
-
-;; Add the user-contributed repositories
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/"))
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")
+                         ("org" . "http://orgmode.org/elpa/")))
 
 ; list the packages you want
 (setq package-list
