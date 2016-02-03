6cb9a68187b9455867be2418baa0bfb4dbb9414a
Aaron Kuehler <aaron.kuehler@gmail.com>
2016-02-03 09:18:25 -0500

    Use web-mode when editing .erb templates

--- configs/web-mode.el
+++ configs/web-mode.el
@@ -1,3 +1,7 @@
 (add-hook 'web-mode-hook
-  (lambda () ""
-    (setq web-mode-markup-indent-offset 2)))
+          (lambda ()
+            ""
+            (setq web-mode-markup-indent-offset 2)))
+
+(add-to-list 'auto-mode-alist
+             '("\\.erb\\'" . web-mode))
