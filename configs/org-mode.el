55440574435dc7484ea818c0410065f031455b7a
Aaron Kuehler <aaron.kuehler@gmail.com>
2016-04-20 11:07:05 -0400

    Enable spell checking in org-mode

--- configs/org-mode.el
+++ configs/org-mode.el
@@ -1,3 +1,5 @@
 (require 'org-bullets)
 (add-hook 'org-mode-hook
-          (lambda () (org-bullets-mode 1)))
+          (lambda ()
+            (org-bullets-mode 1)
+            (flyspell-mode 1)))
