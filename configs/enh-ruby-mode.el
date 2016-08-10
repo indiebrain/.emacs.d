7cb5200f9ad1d09b16019e3900df2387c020968d
Aaron Kuehler <aaron.kuehler@gmail.com>
2016-08-10 14:28:32 -0400

    Use enh-ruby-mode for .rb files

--- configs/enh-ruby-mode.el
+++ configs/enh-ruby-mode.el
@@ -1,3 +1,7 @@
+;; Don't auto indent to context depth,  use the default indent
+(setq enh-ruby-deep-indent-paren nil)
+
+(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . enh-ruby-mode))
