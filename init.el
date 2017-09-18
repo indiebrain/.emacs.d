4932ff1c139359b22078d5a2d77bc774e741eb5d
Aaron Kuehler <aaron.kuehler@gmail.com>
2017-09-18 08:55:14 -0400

    Initialize the package system as early as possible

--- init.el
+++ init.el
@@ -1,2 +1,6 @@
+;; initialize the package system
+(require 'package)
+(package-initialize)
+
 ;; Read emacs configuration from the init org document
 (org-babel-load-file "~/.emacs.d/configuration.org")
