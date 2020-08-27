b8bcec1490aaaa81400b46d6f9b42bbbe599a9ca
Aaron Kuehler <aaron.kuehler@gmail.com>
2020-08-27 08:25:19 -0400

    Update config initialization for Emacs 27+

--- init.el
+++ init.el
@@ -1,6 +1,4 @@
 ;; initialize the package system
-(require 'package)
-(package-initialize)
 
 ;; Read emacs configuration from the init org document
 (org-babel-load-file "~/.emacs.d/configuration.org")
