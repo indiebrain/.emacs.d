fcdb424bf4401e96f26e8022ebfc7837bbc6f7bc
Aaron Kuehler <aaron.kuehler@gmail.com>
2020-11-25 08:51:59 -0500

    Always re-tangle the configuration.org
    Skipping the tangle operation doesn't really save any time at
    startup. Most of the time is spent loading the configuration.el. More
    than once I've forgot to bust-the-configuration.el-cache and spent a
    few minutes wondering how I'd screwed up the emacs-lisp - only to find
    that I just hadn't yet generated the .el with my changes in it.
    
    Too much cost, not enough benefit right now.

--- init.el
+++ init.el
@@ -1,13 +1,4 @@
-;; If the configuration.org has already been tangled into
-;; configuration.el, use the pre-tangled version. Otherwise tangle the
-;; configuration.org. This effectively makes configuration.el a cache
-;; of the state of configuration.org at the time of tangle. To
-;; re-tangle / update the emacs configuration, remove the
-;; configuration.el and restart emacs.
 (let* ((emacs-home "~/.emacs.d/")
-       (configuration-el (concat emacs-home "configuration.el"))
        (configuration-org (concat emacs-home "configuration.org")))
-  (if (file-exists-p configuration-el)
-      (load-file configuration-el)
     ;; Read emacs configuration from the init org document
-    (org-babel-load-file configuration-org)))
+    (org-babel-load-file configuration-org))
